a2f13b95a615ff467ac6bfdfafbb2487a4c19d4f
ale_9702 <alessandroruffolo29@gmail.com>
2024-02-12 15:57:04 +0100

    Configuring pages

---
+++ lib/model/UI/MyApp.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:hotel_frontend/model/UI/pages/Layout.dart';
+
+import '../support/Constants.dart';
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: Constants.APP_NAME,
+      theme: ThemeData(
+        primaryColor: Colors.indigo,
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          background: Colors.white,
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.resolveWith<Color?>(
+                  (Set<MaterialState> states) {
+                if (states.contains(MaterialState.pressed)) {
+                  return Theme.of(context).colorScheme.primary.withOpacity(0.5);
+                }
+                return Colors.lightBlueAccent;
+              },
+            ),
+          ),
+        ),
+      ),
+      home: Layout(title: Constants.APP_NAME), //home e' la prima pagina che viene segnata
+    );
+  }
+}
--- lib/model/support/Constants.dart
+++ lib/model/support/Constants.dart
@@ -1,6 +1,7 @@
 class Constants{
 
+  static final String APP_NAME="FronteEnd-Hotel";
   static final String SERVER_ADDRESS="localhost:8080";
-  static final String REQUEST_NATIONALITY_SCORE="/hello";
+  static final String REQUEST_NATIONALITY_SCORE="/nationalityScore";
 
 }
