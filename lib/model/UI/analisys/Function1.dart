6364b09f3bc80664b01bc7feeeb0b91909c88618
ale_9702 <alessandroruffolo29@gmail.com>
2024-02-12 23:01:09 +0100

    Configuring Function1

---
+++ lib/model/UI/analisys/Function1.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+
+import 'Prova.dart';
+
+class Function1 extends StatefulWidget {
+
+  @override
+  _Function1 createState() => _Function1();
+
+}
+
+class _Function1 extends State<Function1> {
+  String? selectedCountry;
+
+  @override
+  Widget build(BuildContext context) {
+    return HomeScreen();
+  }
+}
---
+++ lib/model/UI/analisys/Prova.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:hotel_frontend/model/Model.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(''),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              showSearch(context: context, delegate: CountrySearchDelegate());
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text('Click on search icon to select a country'),
+      ),
+    );
+  }
+}
+
+class CountrySearchDelegate extends SearchDelegate<String> {
+
+  final List<String> countries = [
+    'USA',
+    'Canada',
+    'UK',
+    'Germany',
+    'France',
+    'Italy',
+    'Spain',
+    // Add more countries as needed
+  ];
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // Actions for the search bar (e.g., clear query button)
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // Leading icon on the left of the search bar (e.g., back button)
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, "");
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // Show results based on the query
+    final List<String> filteredCountries = countries
+        .where((country) => country.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+    return ListView.builder(
+      itemCount: filteredCountries.length,
+      itemBuilder: (BuildContext context, int index) {
+        final String country = filteredCountries[index];
+        return ListTile(
+          title: Text(country),
+          onTap: () {
+            var result = Model().getNationalityNegWords(country);
+
+            close(context, country);
+          },
+        );
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // Show suggestions based on the query (not implemented here)
+    return Center(
+      child: Text('No suggestions yet'),
+    );
+  }
+}
