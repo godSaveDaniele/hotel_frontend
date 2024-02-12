76682be89fe4aea13ef81d478d0ef722a73c7325
ale_9702 <alessandroruffolo29@gmail.com>
2024-02-12 16:07:59 +0100

    Configuring pages

--- lib/model/UI/pages/Analisi.dart
+++ lib/model/UI/pages/Analisi.dart
@@ -1,11 +1,47 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class Analisi extends StatelessWidget {
+class Analisi extends StatefulWidget {
+  @override
+  _Analisi createState() => _Analisi();
+}
+
+class _Analisi extends State<Analisi> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this); // 3 è il numero di schede
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Seleziona la tipologia di analisi che desideri eseguire'),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(text: 'Tab 1'),
+            Tab(text: 'Tab 2'),
+            Tab(text: 'Tab 3'),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          Center(child: Text('Tab 1 content')),
+          Center(child: Text('Tab 2 content')),
+          Center(child: Text('Tab 3 content')),
+        ],
+      ),
+    );
   }
-
 }
--- lib/model/support/Constants.dart
+++ lib/model/support/Constants.dart
@@ -1,6 +1,6 @@
 class Constants{
 
-  static final String APP_NAME="FronteEnd-Hotel";
+  static final String APP_NAME="Gestione Hotel in Europa";
   static final String SERVER_ADDRESS="localhost:8080";
   static final String REQUEST_NATIONALITY_SCORE="/nationalityScore";
 
