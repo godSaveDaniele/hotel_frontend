9055ed69831029dda4a2769b2e9e1e7904e65172
ale_9702 <alessandroruffolo29@gmail.com>
2024-02-16 12:28:01 +0100

    Working on last functionality ...

--- lib/model/UI/booking/BookingFittizio.dart
+++ lib/model/UI/booking/BookingFittizio.dart
@@ -1,5 +1,10 @@
 
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hotel_frontend/model/UI/booking/BarraDiRicercaBooking.dart';
+
+import '../../Model.dart';
+import '../toolsdaniele/BarraDiRicerca.dart';
+import 'HotelBox.dart';
 
 class BookingFittizio extends StatefulWidget {
   @override
@@ -8,10 +13,71 @@ class BookingFittizio extends StatefulWidget {
 }
 
 class _BookingFittizio extends State<BookingFittizio> {
+  GlobalKey<BarraDiRicercaState> childKey = GlobalKey<BarraDiRicercaState>();
+  bool loading = true;
+  late List<Coppia> dati;
+  late List<String> campiSelezionati;
+  late List<String> campiMenuTendina;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    if (loading) {
+      _getData();
+      return Container(
+          child:
+          Center(
+              child: CircularProgressIndicator()
+          )
+      );
+    } else {
+      return contenuto();
+    }
+  }
+
+  Widget contenuto(){
+    List<String>? tmp= childKey.currentState?.getCampiSelezionati();
+    if (tmp==null ){
+      tmp=[];
+    }
+    campiSelezionati=tmp;
+    List<String> datiHotelBox = seleziona(dati, campiSelezionati);
+    return Row(
+        children:[
+          BarraDiRicercaBooking(key: childKey, aggiorna: aggiorna, campiMenuTendina: campiMenuTendina),
+          HotelBox(datiHotelBox)
+        ]
+    );
+  }
+
+  List<String> seleziona(List<Coppia> dati, List<String> campiSelezionati) {
+    List<String> ris= [];
+    for (String tag in campiSelezionati){
+      List<String> hotel = estraiTopHotel(tag,dati);
+      ris.addAll(hotel);
+    }
+    return ris;
+  }
+
+  List<String> estraiTopHotel(String tag, List<Coppia> dati) {
+    List<String> hotels = [];
+    for(Coppia coppia in dati){
+      if(coppia.item2.contains(tag))
+        hotels.add(coppia.item1);
+    }
+    return hotels;
+  }
+
+  Future<void> _getData() async {
+    dati= (await Model.sharedInstance.getHotelTags())!;
+    campiMenuTendina= (await Model.sharedInstance.getAllTags())!;
+    setState(() {
+      loading = false;
+    });
+  }
+
+  aggiorna(){
+    setState(() {
+    });
   }
 
 
---
+++ lib/model/UI/booking/HotelBox.dart
@@ -0,0 +1,64 @@
+
+import 'package:flutter/material.dart';
+
+class HotelBox extends StatelessWidget {
+  late final List<String> _dati;
+
+  HotelBox(List<String> datiHotelBox){
+    _dati = datiHotelBox;
+  }
+
+  Widget build(BuildContext context) {
+    return
+      Container(
+          height:400,
+          width:250,
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.black, // Colore del bordo
+              width: 2.0, // Spessore del bordo
+            ),
+          ),
+          margin:EdgeInsets.all(30) ,
+          child:
+          Scrollbar(
+              child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: _dati.map(
+                          (string) => Container(
+                          padding: EdgeInsets.all(8.0),
+                          margin: EdgeInsets.symmetric(vertical: 4.0),
+                          decoration: BoxDecoration(
+                            color: Colors.red[200],
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          child:
+                          Row(
+                            children:[
+                              Text(
+                                modifyHotelString(string),
+                                style: TextStyle(fontSize: 16.0),
+                              ),
+                            ],
+                          )
+                      ),
+                    ).toList(),
+                  )
+              )
+          )
+
+      );
+  }
+
+  String modifyHotelString(String string) {
+    List<String> parole  = string.split(" ");
+    if (parole.length >= 5) {
+      // Prendo le prime tre parole e le ultime due parole
+      String primaParte = parole.sublist(0, 3).join(" ");
+      String secondaParte = parole.sublist(parole.length - 2).join(" ");
+
+      return '$primaParte, $secondaParte';
+    } else { return ''; }
+  }
+}
